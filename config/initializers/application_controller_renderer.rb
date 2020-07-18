9c21af57605f3531bb1d6fdcfe2b17024d05f3a7
森考喜 <morikouki@morikoukinoMacBook-Pro.local>
2020-07-18 17:08:39 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '112fa0fb645255c9a17e7ec2f3c637169d133956d953a2d3613ab9cc68ea9f23a53c0e68831a8d98aab56b238865fe8347be3028f3fa271216f32ae1f5947187'
